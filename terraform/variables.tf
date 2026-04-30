184a9eaafd37249dfe9fe54586a6dab40f5340a3
Cristopher Paiva <josuepaiva7@gmail.com>
2026-04-30 14:40:37 -0300

    Actualizar la región predeterminada a "europe-west1" en la configuración de Terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,7 +6,7 @@ variable "project_id" {
 variable "region" {
   description = "GCP region"
   type        = string
-  default     = "us-central1"
+  default     = "europe-west1"
 }
 
 variable "cluster_name" {
